8c764bc4f1d6380270917ebacfc337079a8ba6a1
andersonhsilva <anderson.h.silva@gmail.com>
2021-11-07 04:04:28 -0300

    correcao de metodos depreciados troca de {} por () em validacao de cnpj

--- src/Metodos.php
+++ src/Metodos.php
@@ -447,23 +447,25 @@ class Metodos
             return false;
         // Valida primeiro dígito verificador
         for ($i = 0, $j = 5, $soma = 0; $i < 12; $i++) {
-            $soma += $cnpj{
-                $i} * $j;
+            $soma += $cnpj(
+                $i
+            ) * $j;
             $j = ($j == 2) ? 9 : $j - 1;
         }
         $resto = $soma % 11;
-        if ($cnpj{
-            12} != ($resto < 2 ? 0 : 11 - $resto))
+        if ($cnpj(
+            12
+        ) != ($resto < 2 ? 0 : 11 - $resto))
             return false;
         // Valida segundo dígito verificador
         for ($i = 0, $j = 6, $soma = 0; $i < 13; $i++) {
-            $soma += $cnpj{
-                $i} * $j;
+            $soma += $cnpj(
+                $i
+            ) * $j;
             $j = ($j == 2) ? 9 : $j - 1;
         }
         $resto = $soma % 11;
-        return $cnpj{
-            13} == ($resto < 2 ? 0 : 11 - $resto);
+        return $cnpj(13) == ($resto < 2 ? 0 : 11 - $resto);
     }
 
     public function imagem_thumbnails($new_width, $new_height, $source_file, $dst_file, $quality = 60)
